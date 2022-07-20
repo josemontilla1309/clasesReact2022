c17ed9512b690d90bf615e5a032271840a0d7158
Jose <josemontilla1309@gmail.com>
2022-07-19 21:44:47 -0400

    agregando navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return(
+        <div>
+            <h1>FlightHouse</h1>
+            <ul>
+                <li><button>Vuelos</button></li>
+                <li><button>Descuentos</button></li>
+                <li><button>Contactanos</button></li>
+                <li><button>RRSS</button></li>
+            </ul>
+        </div>
+    )
+} 
+
+export default NavBar
